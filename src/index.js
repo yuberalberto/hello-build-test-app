41f5ceadc4e8e2ad946d7aaa4a030ff53e805336
yuberalberto <yuberalberto@gmail.com>
2021-08-22 16:30:46 -0500

    Added the home route to the project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
+import App from './routes/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
